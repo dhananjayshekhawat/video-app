aa9f4b188653223b3a3463ae4b15cbd2c80200b3
dhananjayshekhawat <20je0321@me.iitism.ac.in>
2025-09-10 14:49:28 +0530

    Api developing

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
 dotenv.config({ path: "./env" });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000),
+      () => {
+        console.log("Server is running at port ");
+      };
+  })
+  .catch((err) => {
+    console.log("MongoDB db connection failed  ", err);
+  });
