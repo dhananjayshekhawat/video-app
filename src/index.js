eececc83ea1c18e3b1fc6eeabbccb9c3d68e1592
dhananjayshekhawat <20je0321@me.iitism.ac.in>
2025-09-12 15:58:31 +0530

    add apii

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import dotenv from "dotenv";
-
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
 dotenv.config({ path: "./env" });
+
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000),
-      () => {
-        console.log("Server is running at port ");
-      };
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, () => {
+      console.log(`✅ Server is running at port ${PORT}`);
+    });
   })
   .catch((err) => {
-    console.log("MongoDB db connection failed  ", err);
+    console.log(" MongoDB db connection failed", err);
   });
